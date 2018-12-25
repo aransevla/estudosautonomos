c01c7d46f38e96405f70c40b2ee1a3631d075f61
Nara <zumiizombie@gmail.com>
2018-12-25 11:01:13 -0200

    Fazendo o player pular (falta tratar pulo infinito)

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 10f;
+	public float jumpSpeed=600f;
 	Rigidbody2D rb;
 
 
@@ -24,6 +25,9 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 		else {
 			StopMovingHorizontal();
+	}
+		if (Input.GetButtonDown("Jump")) {
+			Jump();
 		}
 	}
 
@@ -35,4 +39,8 @@ public class PlayerCtrl : MonoBehaviour {
 	void StopMovingHorizontal () {
 		rb.velocity=new Vector2(0f, rb.velocity.y);
 		}
+		void Jump () {
+		rb.AddForce(new Vector2(0f, jumpSpeed));
 	}
+}
+	
