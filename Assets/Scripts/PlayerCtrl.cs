4c91f8e80818e8c08bb6fa2c2ba2624bce2217d3
Nara <zumiizombie@gmail.com>
2018-12-25 13:03:44 -0200

    Controlando as animacoes

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+	Idle - 0
+	Jump - 1
+	Run - 2
+	Falling - 3
+	Shooting - 4
+	Hurt - 5
+ */
 public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 10f;
 	public float jumpSpeed=600f;
 	Rigidbody2D rb;
 	SpriteRenderer sr;
+	Animator anim;
+
+	bool isJumping=false;
 
 	// Use this for initialization
 	void Start () {
 
 		rb = GetComponent<Rigidbody2D>();
 		sr=GetComponent<SpriteRenderer>();
+		anim=GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -30,6 +42,7 @@ public class PlayerCtrl : MonoBehaviour {
 		if (Input.GetButtonDown("Jump")) {
 			Jump();
 		}
+			ShowFalling();
 	}
 
 	void MoveHorizontal(float speed) {
@@ -40,13 +53,32 @@ public class PlayerCtrl : MonoBehaviour {
 		else if (speed>0f){
 			sr.flipX=false;
 		}
+		if (!isJumping) {
+		anim.SetInteger("State", 2);
+		}
 	}
 
 	void StopMovingHorizontal () {
 		rb.velocity=new Vector2(0f, rb.velocity.y);
+		if (!isJumping) {
+		anim.SetInteger("State",0);
+		}
+	}
+
+	void ShowFalling() {
+		if (rb.velocity.y<0f) {
+			anim.SetInteger("State",3);
+		}
 		}
 		void Jump () {
+			isJumping=true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State",1);
+	}
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.layer==LayerMask.NameToLayer("Ground")) {
+			isJumping=false;
+		}
 	}
 }
 	
