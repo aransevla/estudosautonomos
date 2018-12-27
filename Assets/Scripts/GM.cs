cf910f7b935f1a8db848974b5b5b3b3e7ca71114
Nara <zumiizombie@gmail.com>
2018-12-27 18:23:23 -0200

    Contando vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -58,6 +58,7 @@ public class GM : MonoBehaviour {
 
 	void DisplayHudData() {
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtLifeCount.text="x "+data.lifeCount;
 		ui.hud.txtTimer.text="Timer: "+timeLeft.ToString("F1");
 	}
 	public void IncrementCoinCount() {
@@ -67,11 +68,19 @@ public class GM : MonoBehaviour {
 	public void RespawnPlayer() {
 		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
 	}
-
+	public void DecrementLives() {
+		data.lifeCount--;
+	}
 	public void KillPlayer() {
 		 if (player!=null) {
 			 Destroy(player.gameObject);
+			  DecrementLives();
+			 if (data.lifeCount>0) {
 			 Invoke("RespawnPlayer", timeToRespawn);
+		 	}
+			  else {
+				 GameOver(); 
+			 }
 		 }
 	}
 	public void ExpirePlayer() {
