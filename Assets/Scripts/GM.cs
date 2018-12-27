b485fffc26f97ccede8c77d58b6e8bab6d766d0f
Nara <zumiizombie@gmail.com>
2018-12-27 17:24:13 -0200

    Timer

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -11,6 +11,10 @@ public class GM : MonoBehaviour {
 	public GameObject playerPrefab;
 	PlayerCtrl player;
 	public float timeToRespawn = 2f;
+
+	public float maxTime=60f;
+	bool timerOn=true;
+	float timeLeft;
 	public UI ui;
 	GameData data=new GameData();
 
@@ -27,6 +31,7 @@ public class GM : MonoBehaviour {
 		if (player==null) {
 			RespawnPlayer();
 		}
+		timeLeft=maxTime; 
 	}
 	
 	// Update is called once per frame
@@ -37,10 +42,23 @@ public class GM : MonoBehaviour {
 				player =obj.GetComponent<PlayerCtrl>();
 			}
 		}
+		UpdateTimer();
 		DisplayHudData();
 	}
+
+	void UpdateTimer() {
+		if (timerOn) {
+			timeLeft=timeLeft-Time.deltaTime;
+			if (timeLeft<=0f) {
+				timeLeft=0;
+				ExpirePlayer();
+				}	
+			}
+		}
+
 	void DisplayHudData() {
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtTimer.text="Timer: "+timeLeft.ToString("F1");
 	}
 	public void IncrementCoinCount() {
 		data.coinCount++;
@@ -55,5 +73,18 @@ public class GM : MonoBehaviour {
 			 Destroy(player.gameObject);
 			 Invoke("RespawnPlayer", timeToRespawn);
 		 }
+	}
+	public void ExpirePlayer() {
+		 if (player!=null) {
+			 Destroy(player.gameObject);
+			 
+		 }
+		 GameOver();
+	}
+	void GameOver () {
+		timerOn=false;
+		ui.gameOver.txtCoinCount.text="Coins: " +data.coinCount;
+		ui.gameOver.txtTimer.text="Timer: "+timeLeft.ToString("F1");
+		ui.gameOver.gameOverPanel.SetActive(true);
 	}
 }
